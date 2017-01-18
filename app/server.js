dfc8bd3e4cd12ca464a3a48db16298c364c439db
Michael Foss <michael.foss@valtech.com>
2017-01-18 12:39:57 -0500

    Add initial Pug view

--- app/server.js
+++ app/server.js
@@ -8,6 +8,9 @@ app.use(express.static(__dirname + '/../www'));
 // Define views folder
 app.set('views', __dirname + '/views');
 
+// Define view engine
+app.set('view engine', 'pug');
+
 // Route the default request
 app.get('/', (req, res) => {
     res.render('index');
