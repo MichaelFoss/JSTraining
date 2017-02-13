06cf0451d81331b674f82b2d3a63b1e288766f77
Michael Foss <michael@mikefoss.com>
2017-02-13 08:42:58 -0500

    Convert to initial Tour Of Heroes site
    Convert the QuickStart site to the Tour Of Heroes site.
    Include a hero, as well as a way to edit it.

--- Angular2-Tour-Of-Heroes/src/app/app.component.ts
+++ Angular2-Tour-Of-Heroes/src/app/app.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{currentHero.name}} Details</h2>
+    <div>
+      <label>id: </label>{{currentHero.id}}
+    </div>
+    <div>
+      <label>name: </label>
+      <input type="text" [(ngModel)]="currentHero.name" placeholder="Name" />
+    </div>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  {
+  title = 'Tour Of Heroes';
+  currentHero = {
+    id: 1,
+    name: 'Storm'
+  };
+}
