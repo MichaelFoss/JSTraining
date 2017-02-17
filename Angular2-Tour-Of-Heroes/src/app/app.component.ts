1935620e926a386098841603ca82d4a0ac22ccbc
Michael Foss <michael@mikefoss.com>
2017-02-17 14:37:47 -0500

    Move mock hero data to separate file

--- Angular2-Tour-Of-Heroes/src/app/app.component.ts
+++ Angular2-Tour-Of-Heroes/src/app/app.component.ts
@@ -1,18 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  { id: 1, name: 'Storm' },
-  { id: 2, name: 'Wolverine' },
-  { id: 3, name: 'Cyclops' },
-  { id: 4, name: 'Beast' },
-  { id: 5, name: 'Nightcrawler' },
-  { id: 6, name: 'Cable' },
-  { id: 7, name: 'Dazzler' },
-  { id: 8, name: 'Iceman' },
-  { id: 9, name: 'Rogue' },
-  { id: 10, name: 'Jean Grey' }
-];
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'my-app',
---
+++ Angular2-Tour-Of-Heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Storm' },
+  { id: 2, name: 'Wolverine' },
+  { id: 3, name: 'Cyclops' },
+  { id: 4, name: 'Beast' },
+  { id: 5, name: 'Nightcrawler' },
+  { id: 6, name: 'Cable' },
+  { id: 7, name: 'Dazzler' },
+  { id: 8, name: 'Iceman' },
+  { id: 9, name: 'Rogue' },
+  { id: 10, name: 'Jean Grey' }
+];
