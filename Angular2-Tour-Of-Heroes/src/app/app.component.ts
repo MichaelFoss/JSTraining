f5ebd5156b23d8bcdc139cc022d21c2cf32d6d9f
Michael Foss <michael@mikefoss.com>
2017-02-17 15:09:29 -0500

    Retrieve hero data asynchronously

--- Angular2-Tour-Of-Heroes/src/app/app.component.ts
+++ Angular2-Tour-Of-Heroes/src/app/app.component.ts
@@ -79,7 +79,8 @@ export class AppComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
