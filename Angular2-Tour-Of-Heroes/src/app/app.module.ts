4ce909d3f560ff6c1352895c95c6e1305cd1bda3
Michael Foss <michael@mikefoss.com>
2017-02-28 02:48:28 -0500

    Refactor routing to routing module
    Move the routing logic & data from the main app module to a separate
    routing module.

--- Angular2-Tour-Of-Heroes/src/app/app.module.ts
+++ Angular2-Tour-Of-Heroes/src/app/app.module.ts
@@ -1,36 +1,18 @@
-import { NgModule }      from '@angular/core';
-import { RouterModule }  from '@angular/router';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroService } from './hero-service';
-import { HeroesComponent }  from './heroes.component';
-import { HeroDetailComponent } from "./hero-detail.component";
 import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent }  from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/',
-        pathMatch: 'full'
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
