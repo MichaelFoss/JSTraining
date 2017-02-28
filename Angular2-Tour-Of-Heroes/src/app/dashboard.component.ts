5cb5c2f0a3a7f9cff08a7f0e29695a3eced49bc4
Michael Foss <michael@mikefoss.com>
2017-02-27 19:06:07 -0500

    Provide top heroes in dashboard
    Move the template markup to a separate HTML file. Cherry-pick heroes
    2-5 to use as the top heroes in the dashboard component.

--- Angular2-Tour-Of-Heroes/src/app/dashboard.component.ts
+++ Angular2-Tour-Of-Heroes/src/app/dashboard.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero-service';
+import { DELAY } from './mock-heroes';
 
 @Component({
+  moduleId: module.id,
   selector: 'Dashboard',
-  template: `
-    <h1>Dashboard</h1>
-  `
+  templateUrl: 'dashboard.component.html'
 })
 
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.heroService.getHeroes(DELAY)
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
 }
